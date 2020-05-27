96a037d3ad10f07ec2114fa4bd9375e05de3fd39
Ann Philip <aphilip@uoregon.edu>
2020-05-26 21:50:04 -0700

    Animals

--- client/javascripts/Animal.js
+++ client/javascripts/Animal.js
@@ -47,11 +47,23 @@ class Rabbit extends Animal {
 
 // Inherit from Animal
 class Dog extends Animal {
-  //your code goes here..
+  constructor(name, breed, weight, bark)  {
+    super(name);
+    this.breed = breed;
+    this.weight = weight;
+    this.bark = bark;
+  }
+
+  hide()  {
+    console.log(`${this.name} hides!`);
+  }
+
+  stop() {
+    this.speed = 0;
+    console.log(`${this.name} stopped.`);
+  }
 
 }
-
-let test_driver = () => {
 
   let test_rabbit = () => {
     let rabbit = new Rabbit("White Rabbit", 10);
@@ -66,17 +78,17 @@ let test_driver = () => {
     let fang = new Dog("Fang", "Airedale", 50, "Woof");
     fang.run(20);
     console.log(`${fang.name} is a ${fang.breed} dog weighing ${fang.weight} lbs. that can run ${fang.speed} mph.`);
-    console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+    console.log(`Look, a cat! ${fang.name} barks: ${fang.bark} ${fang.bark}!`);
     fang.stop();
+    console.log(`${fang.bark} ${fang.bark}`);
     const  zero = new Dog("Zero", "Beagle", 22, "Yip" );
     zero.run(10);
     console.log(`${zero.name} is a ${zero.breed} dog weighing ${zero.weight} lbs. that can run ${zero.speed} mph.`);
-    console.log(`Look, a cat! ${zero.name} barks: ${zero.bark()}`);
+    console.log(`Look, a cat! ${zero.name} barks: ${zero.bark} ${zero.bark}!`);
     zero.stop();
-
+    console.log(`${zero.bark} ${zero.bark}!`);
   }
 
   //run tests
   test_rabbit();
   test_dog();
-}
